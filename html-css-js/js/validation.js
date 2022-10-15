8387ff9e0290510ca61cca9b390616ccd698c156
Julo-Coder <julokozlowskipriv@gmail.com>
2022-10-15 21:24:03 +0200

    Adding validation empty fields

--- html-css-js/js/validation.js
+++ html-css-js/js/validation.js
@@ -9,3 +9,27 @@
     Powodzenia!
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 */
+
+const nameInput = document.querySelector('#first-name');
+const lastNameInput = document.querySelector('#last-name');
+const emailInput = document.querySelector('#email');
+const btn = document.querySelector(".btn");
+
+const checkForm = () =>{
+    event.preventDefault()
+    if(nameInput.value == "" && lastNameInput.value == "" && emailInput.value == ""){
+        console.log("Puste inputy");
+    }else if(nameInput.value == "" || lastNameInput.value == "" || emailInput.value == "" ){
+        let allInputValues = []
+        allInputValues.push(nameInput)
+        allInputValues.push(lastNameInput)
+        allInputValues.push(emailInput)
+        for(i of allInputValues){
+            if(i.value == ""){
+                console.log(i);
+            }
+        }
+    }
+    
+
+}
