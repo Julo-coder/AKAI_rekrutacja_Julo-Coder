c6df9778fee14ca4d4ff8de7f28894a99367e46b
Julo-Coder <julokozlowskipriv@gmail.com>
2022-10-23 14:26:27 +0200

    Finish tic-tac-toe project.

--- js/tic-tac-toe/index.js
+++ js/tic-tac-toe/index.js
@@ -34,13 +34,50 @@ function displayTurn(turn) {
 
 function checkWin() {
   // 2. sprawdź czy któryś z graczy wygrał pojedynek - jeśli tak wyświetla komunikat (możesz użyć np. funkcji "alert(...)")
+  // console.log(symbols[0][0], symbols[1][0], symbols[2][0]);
+  // console.log(symbols[0][1], symbols[1][1], symbols[2][1]);
+  // console.log(symbols[0][2], symbols[1][2], symbols[2][2]);
+  console.log(symbols);
+  if(symbols[0][0] == "x" && symbols[0][1] == "x" && symbols[0][2] == "x"){
+    alert("X win");
+  }else if(symbols[0][0] == "o" && symbols[0][1] == "o" && symbols[0][2] == "o"){
+    alert("O win");
+  }else if(symbols[1][0] == "x" && symbols[1][1] == "x" && symbols[1][2] == "x"){
+    alert("X win");
+  }else if(symbols[1][0] == "o" && symbols[1][1] == "o" && symbols[1][2] == "o"){
+    alert("O win");
+  }else if(symbols[2][0] == "x" && symbols[2][1] == "x" && symbols[2][2] == "x"){
+    alert("X win");
+  }else if(symbols[2][0] == "o" && symbols[2][1] == "o" && symbols[2][2] == "o"){
+    alert("O win");
+  }else if(symbols[0][0] == "o" && symbols[1][0] == "o" && symbols[2][0] == "o"){
+    alert("O win");
+  }else if(symbols[0][0] == "x" && symbols[1][0] == "x" && symbols[2][0] == "x"){
+    alert("X win");
+  }else if(symbols[0][1] == "o" && symbols[1][1] == "o" && symbols[2][1] == "o"){
+    alert("O win");
+  }else if(symbols[0][1] == "x" && symbols[1][1] == "x" && symbols[2][1] == "x"){
+    alert("X win");
+  }else if(symbols[0][2] == "x" && symbols[1][2] == "x" && symbols[2][2] == "x"){
+    alert("X win");
+  }else if(symbols[0][2] == "o" && symbols[1][2] == "o" && symbols[2][2] == "o"){
+    alert("O win");
+  }else if(symbols[0][0] == "x" && symbols[1][1] == "x" && symbols[2][2] == "x"){
+    alert("X win");
+  }else if(symbols[0][0] == "o" && symbols[1][1] == "o" && symbols[2][2] == "o"){
+    alert("O win");
+  }else if(symbols[0][2] == "o" && symbols[1][1] == "o" && symbols[2][0] == "o"){
+    alert("O win");
+  }else if(symbols[0][2] == "x" && symbols[1][1] == "x" && symbols[2][0] == "x"){
+    alert("X win");
+  }
 }
+
 
 // 3. dodaj listener pod przycisk z napisaem "reset" tak, aby po jego kliknięciu wywołać funkcję reset
 function reset() {
   // 4. zresetuj stan gry
-
-
+  window.location.reload()
 }
 
-resetBtn.addEventListener("click", reset())
+resetBtn.addEventListener("click", reset)
